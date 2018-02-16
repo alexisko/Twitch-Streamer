34a8dc83fccd56670f585b2699b2ad11af2e57e1
Alexis <alexisko@hawaii.edu>
2018-02-16 10:44:21 -1000

    Sorting working, need to work on styling of streams

--- main.js
+++ main.js
@@ -2,23 +2,53 @@ $(document).ready(function() {
   var url = 'https://wind-bow.gomix.me/twitch-api/';
   var channels = ["ESL_SC2", "OgamingSC2", "cretetion",
   "freecodecamp", "storbeck", "habathcx", "RobotCaleb",
-  "noobs2ninjas"];
-  channels.forEach(function(name) {
-    $.getJSON(url + 'streams/' + name + '?callback=?').success(function(data) {
-      console.log(data);
-      var isStreaming = (data.stream !== null) ? true : false;
-      if(isStreaming) {
-        var online = displayStream(data);
-        $('.online').append(online);
-      } else {
-        var offline = offlineStream(name);
-        $('.offline').append(offline);
-      }
-    });
+  "noobs2ninjas", "brunofin"];
+
+  var activeBtn = 'all';
+  getStreamData();
+
+  $('.btn').click(function() {
+    $('.btn').removeClass('active');
+    $(this).toggleClass('active');
+    activeBtn = $(this).val();
+    getStreamData();
+    console.log(activeBtn);
   });
 
-  function displayStream(data) {
-    var img = '<img src="' + data.stream.preview.medium +'">';
+  function getStreamData() {
+    clearResults();
+    channels.forEach(function(name) {
+      $.getJSON(url + 'streams/' + name + '?callback=?').success(function(data) {
+        var isStreaming = (data.stream !== null) ? true : false;
+        // Get logo
+        $.getJSON(url + 'users/' + name + '?callback=?').success(function(user) {
+          console.log(user);
+          if(user.hasOwnProperty('error') && activeBtn === 'all') {
+            $('.container').append(nullStream(name));
+          } else {
+            if(activeBtn === 'all') {
+              $('.container').append(isStreaming ? displayStream(data, user.logo) : offlineStream(name, user.logo));
+            } else if(activeBtn === 'online') {
+              if(isStreaming) {
+                $('.container').append(displayStream(data, user.logo));
+              }
+            } else {
+              if(!isStreaming && !user.hasOwnProperty('error')) {
+                $('.container').append(offlineStream(name, user.logo));
+              }
+            }
+          }
+        });
+      });
+    });
+  }
+
+  function clearResults() {
+    $('.container').empty();
+  }
+
+  function displayStream(data, logo) {
+    var img = '<img src="' + logo +'">';
     var status = '<p>' + data.stream.game + ': ' + data.stream.channel.status + '</p>';
     var link = 'https://www.twitch.tv/' + data.stream.channel.display_name;
     var channel = '<a href="' + link + '"><span class="stream-title">' +
@@ -27,12 +57,20 @@ $(document).ready(function() {
     return stream;
   }
 
-  function offlineStream(name) {
-    var img = '<div class="no-img"></div>';
+  function offlineStream(name, logo) {
+    var img = '<img src="' + logo +'">';
     var link = 'https://www.twitch.tv/' + name;
     var channel = '<a href="' + link + '"><span class="stream-title">' +
     name + '</span></a>';
     var stream = '<div class="stream">' + img + channel + '</div>';
+    return stream;
+  }
+
+  function nullStream(name) {
+    var img = '<div class="no-img"></div>';
+    var channel = '<span class="stream-title">' + name + '</span>';
+    var desc = '<p>Channel not available.</p>';
+    var stream = '<div class="stream">' + img + channel + desc + '</div>';
     return stream;
   }
 });
